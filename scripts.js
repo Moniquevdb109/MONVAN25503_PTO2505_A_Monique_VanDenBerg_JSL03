80ad8b0dd278aa594337806cc97b75c4d74eeb9c
Monique Van Den Berg <moniquevdb109@gmail.com>
2025-10-17 14:37:02 +0200

    updated comments to reflect clear messages

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-// Store tasks as objects inside an array for structured data management.
-
+//Storing tasks as objects inside an array for structured data management.
 const initialTasks = [
   {
     id: 1,
@@ -22,17 +21,18 @@ const initialTasks = [
   },
 ];
 
-// Allow users to add up to three new tasks to the existing task list.
+//while loop to keep adding up to three new tasks to the existing task list.
 while (initialTasks.length < 6) {
   addTasks();
 }
 
-// Alert users when they reach the task limit with a message
+//if statement to alert users when they reach the task limit with a message.
 if (initialTasks.length === 6) {
   alert("There are enough tasks on your board, please check them in the console.")
 }
 
-// Prompt users to enter task details (title, description, status) and store them in an object.
+//function to prompt the users to enter task details and store them in an object.
+//followed by a while loop to validate the task status input and an alent if the status entered is incorrect.
 function addTasks () {
   const taskTitle = prompt("Enter the task title");
   const taskDescription = prompt("Enter the task description");
@@ -46,7 +46,7 @@ function addTasks () {
     alert("Invalid status. Please enter 'todo', 'doing' or 'done'")
     taskStatus = prompt("Enter the task status (todo, doing, done)").toLowerCase();
   }
-// Ensure each new task has a unique incremental ID based on the last task in the array.
+//Ensuring each new task has a unique incremental ID and adds the new task to the back of the existing task list.
   const newTask = {
     id: initialTasks.length + 1,
     title: taskTitle,
@@ -57,13 +57,13 @@ function addTasks () {
   initialTasks.push(newTask);
 }
 
-//  Implement a filter function to display only tasks with the status "done".
+// Implementing a filter function to display only tasks with the status "done".
 const getCompletedTasks = () => 
   initialTasks.filter((task) => task.status === "done");
 
 
-// Log all tasks in the console with a clear label for easy review.
+//Logging all tasks in the console with a clear label.
 console.log("All Tasks:", initialTasks);
 
-// Log only completed tasks (status: "done") in the console under a "Completed Tasks" label for quick reference.
+//Logging only completed tasks (status: "done") in the console under a "Completed Tasks" label.
 console.log("Completed Tasks:", getCompletedTasks());
